59ddc67fb778da6b8d33db13e14a8ea8e289a732
tskira <tskira@hotmail.com>
2018-09-11 16:08:34 -0300

    ex 1

---
+++ exercicio_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// 1) Defina a funcao soma
+int soma(int x, int y) {
+    return x+y;
+}
+
+// 2) Compile o codigo e suba as modificações para o repositorio origin
+
+// 3) Nao permitir que arquivos com extensao .out sejam commitados 
+
+int main() {
+    printf("3 + 2 = %d\n", soma(3, 2));
+    return 0;
+}
